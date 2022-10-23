7012c156a83c2f9371bf1c6de73cc8439a19e2e8
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <96749372+a0zhar@users.noreply.github.com>
2022-10-23 11:41:35 +0200

    Update FiletoBytes.cs

--- FiletoBytes.cs
+++ FiletoBytes.cs
@@ -3,22 +3,15 @@ using System.IO;
 namespace ConsoleApp4 {
     internal class Program {
         private static void Main(string[] args) {
-            Console.Title = "Simple C# Console (File to Byte Array) Converter - made by AzharCQ";
-
+            Console.Title = "Simple C# Console (File to Byte Array) Converter - made by a0zhar";
             Console.Write("Drag and Drop file here :-");
-            string path2File = Console.ReadLine();
-            byte[] bytes = File.ReadAllBytes(path2File);
-
-            Console.Write("Include '0x' before bytes? > (Y/N) :");
-            string include0x = Console.ReadLine();
-
+         
+            byte[] bytes = File.ReadAllBytes(Console.ReadLine());
             Console.Clear();
-            int i = 0;
 
             foreach (byte o in bytes) {
-                Console.Write($"{(include0x == "Y" ? "0x" : "")}{o}{(++i < bytes.Length ? ", " : "")}");
+                Console.Write(o + ",");
             }
-
             Console.ReadKey();
 
         }
